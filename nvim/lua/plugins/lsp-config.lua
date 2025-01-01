16dc3a3fa6a68c2d0789a3840598432cbc4dbb02
Kevin Hernot <heavykevin2000@hotmail.com>
2025-01-01 21:20:05 +0700

    Updated lua_ls config to recognize vim

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -21,6 +21,23 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({
         capabilities = capabilities,
+        settings = {
+          Lua = {
+            runtime = {
+              version = "LuaJIT",
+            },
+            diagnostics = {
+              globals = { "vim" },
+            },
+            workspace = {
+              library = vim.api.nvim_get_runtime_file("", true),
+              checkThirdParty = false,
+            },
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
       })
       lspconfig.gopls.setup({
         capabilities = capabilities,
